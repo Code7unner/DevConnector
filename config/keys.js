600e5945ae109ec0a661d29c22ccebb464752d8c
Soul7ucker <cherbadzhimaxim@gmail.com>
2019-01-11 17:39:27 +1000

    Add hosting the project

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://Max:mos1monavt@ds141024.mlab.com:41024/developmentconnector',
-    secretOrKey: `secret`
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://Max:mos1monavt@ds141024.mlab.com:41024/developmentconnector',
+    secretOrKey: `secret`
+};
